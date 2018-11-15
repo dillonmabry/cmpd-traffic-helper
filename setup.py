c5b5c746111bf82e96bc03f1c7ac150f00a11ee0
Dillon Mabry <dcmabry@gmail.com>
2018-11-15 17:33:35 -0500

    Format/beautify

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
-from setuptools import setup
+from setuptools
+import setup
 
-setup(name='cmpd_accidents',
-	version='0.1.0',
-	description='Get CMPD Traffic Data via SOAP GIS Service',
-	author='Dillon Mabry',
-	author_email='rapid.dev.solutions@gmail.com',
-	license='MIT',
-	packages=['cmpd_accidents'],
-	test_suite='nose.collector',
-	tests_require=['nose'],
-	install_requires=['pymongo','requests', 'bs4'],
-	include_package_data=True,
-	data_files=[('',
-		[
-			'cmpd_accidents/resources/cmpd_soap_descriptor.xml'
-		])
-	],
-	zip_safe=False)
+setup(name = 'cmpd_accidents',
+    version = '0.1.0',
+    description = 'Get CMPD Traffic Data via SOAP GIS Service',
+    author = 'Dillon Mabry',
+    author_email = 'rapid.dev.solutions@gmail.com',
+    license = 'MIT',
+    packages = ['cmpd_accidents'],
+    test_suite = 'nose.collector',
+    tests_require = ['nose'],
+    install_requires = ['pymongo', 'requests', 'bs4'],
+    include_package_data = True,
+    data_files = [('', [
+        'cmpd_accidents/resources/cmpd_soap_descriptor.xml'
+    ])],
+    zip_safe = False)
