dd95dbd1630a20c5be8186c2698846da2c7cf488
dillonmabry <dcmabry@gmail.com>
2019-01-05 23:45:06 -0500

    Fix build with appropriate dependencies

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name = 'charlotte_traffic_analysis',
     packages = ['cmpd_accidents', 'traffic_analyzer'],
     test_suite = 'nose.collector',
     tests_require = ['nose'],
-    install_requires = ['pymongo', 'requests', 'lxml', 'bs4', 'sqlalchemy', 'pymysql', 'numpy', 'pandas', 'scikit-learn', 'shapely'],
+    install_requires = ['pymongo', 'requests', 'lxml', 'bs4', 'sqlalchemy', 'pymysql', 'numpy', 'pandas', 'scikit-learn', 'xgboost', 'shapely'],
     include_package_data = True,
     data_files = [('', [
         'cmpd_accidents/resources/soap_descriptors/cmpd_soap_descriptor.xml',
